8d3eb135aa875dc2475d6b88ead29076906dbb67
Lookuz <cweyyeh512@gmail.com>
2019-06-19 11:25:22 +0800

    Load weights from persistent file

--- model_stacking.py
+++ model_stacking.py
@@ -1,6 +1,6 @@
 import pandas as pd
 import write_csv
-from ai_cloud_etl import data_extract_e, data_filter, feature_eng, feature_transform, extract_queues, fit_labels
+from ai_cloud_etl import data_extract_e, data_filter, feature_eng, feature_transform, extract_queues, load_labels, load_data
 import numpy as np
 from sklearn import preprocessing
 from sklearn import metrics
@@ -21,20 +21,39 @@ df = data_extract_e('e_20190609_15.pkl')
 # Data Transformation and Engineering
 df = feature_eng(df)
 df = extract_queues(df)
-dept_encoder, queue_encoder = fit_labels(df)
+dept_encoder, queue_encoder = load_labels('dept_encoder.pkl', 'queue_encoder.pkl', df=df)
 df = feature_transform(df, dept_encoder=dept_encoder, queue_encoder=queue_encoder)
 
 # Training/Test Split
 x, y = data_filter(df)
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=2468)
 
-# Models
-# TODO: Load weights and hyperparameters for each model from file once tuned.
-rf = RandomForestRegressor(n_estimators=50, n_jobs=-1)
-svr = SVR(kernel='rbf', C=100, gamma=0.1, n_jobs=-1)
-xgb = XGBRegressor(objective='reg:linear', n_estimators=100, learning_rate=0.1)
-cb = CatBoostRegressor()
-gbr = GradientBoostingRegressor(n_jobs=-1)
+# Load models from persistent files
+try:
+   rf = load_data('rf.pkl')
+except:
+   rf = RandomForestRegressor(n_estimators=50, n_jobs=-1)
+
+try:
+    svr = load_data('svr.pkl')
+except:
+    svr = SVR(kernel='rbf', C=100, gamma=0.1)
+
+try:
+    xgb = load_data('xgb.pkl')
+except:
+    xgb = XGBRegressor(objective='reg:linear', n_estimators=100, learning_rate=0.1)
+
+try:
+    cb = load_data('cb.pkl')
+except:
+    cb = CatBoostRegressor()
+
+try:
+    gbr = load_data('gbr.pkl')
+except:
+    gbr = GradientBoostingRegressor()
+
 models = [rf, svr, xgb, cb, gbr]
 
 # Stacking
