46f461dedf96d23da218ce4803ce30220c0be548
Lookuz <cweyyeh512@gmail.com>
2019-06-28 10:16:21 +0800

    Added subroutine script for parsing default memory allocation for each node

---
+++ mem_extract.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Subroutine script that extracts the maximum memory of a node in each queue
+# Maximum memory will be used as the default assigned memory to a job script 
+# in the absence of the memory requested field
+
+# Queues
+declare -a queues=(
+    "gpu" "serial" "short" "parallel8" "parallel12" "parallel20" "parallel24"
+)
+
+for i in "${queues[@]}"; do
+    check_pbs_nodes "$i" | grep -iw "$i" | head -1 | sed "s/^.*\($i\)/\1/" | awk '{$1=$1;printf("%s %s\n", $1, $2)}'
+done
