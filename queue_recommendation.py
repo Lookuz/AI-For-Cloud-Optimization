ddbcd253f501c1254b1ec3bf9c81e8dcca7326b0
Lookuz <cweyyeh512@gmail.com>
2019-07-01 16:12:11 +0800

    Added base recommendation function for queue_recommendation module

--- queue_recommendation.py
+++ queue_recommendation.py
@@ -1,38 +1,71 @@
 import os
 import subprocess
 import sys
+import json
+from math import ceil, floor
+import queue_extract
+from recommendation_global import QUEUE_EXTRACT, MEM_EXTRACT, DEFAULT_FILE_NAME, DEFAULT_CPU, MIN_CHUNK
 
 # Script that approximates the current load of each queue based on current job statistics per queue
 # TODO: define load metric
 
-QUEUE_PROC_FILE = './queue_processed.txt' # queue processing file to be executed too obtain intermediate results
-QUEUE_PROC_SCRIPT = './queue_extract.sh' # queue processing script to obtain intermediate log file
-REFRESH_KEYWORD = 'update'
+# Loads the default parameters and values for each of the queues
+def load_defaults():
+    if (not os.path.exists(DEFAULT_FILE_NAME)): # Default file missing, run queue_extract subroutine
+        q_default = queue_extract.get_queue_default(queue_extract=QUEUE_EXTRACT, mem_extract=MEM_EXTRACT)
+        queue_extract.save_default(q_default)
+    else: # Load from existing file
+        with open(DEFAULT_FILE_NAME, 'r') as default_file:
+            q_default = json.load(default_file)
+
+    return q_default
+
+q_default = load_defaults()
+
+# Function that recommends the number of CPUs and nodes for a job
+# based on the predicted CPU utilization and the queue settings
+# returns (select, ncpus)
+def recommend_cpu(est_cpu, queue):
+    try:
+        min_cpu = q_default[queue][DEFAULT_CPU]
+        min_chunk = q_default[queue][MIN_CHUNK]
+
+        if est_cpu - min_cpu < 0:
+            return 1, min_cpu
+        else:
+            threshold = 0.2 # threshold for rounding down instead of up
+            select = est_cpu/float(min_cpu)
+            node_eff_bneck = select - floor(select)
+            if node_eff_bneck < threshold:
+                select = floor(select)
+            else:
+                select = ceil(select)
+                
+            return select, min_cpu
+    except KeyError as e: # TODO: handle missing values
+        pass
+
+
+# Function that evaluates the efficiency of using a queue if a job script is assigned to it
+# Formula used for the evaluation measures the 1) bottlenecking node efficiency and 2) number of nodes used
+# and aims to reduce node usage while increasing bottlenecking node efficiency
+# Uses the following metric for node efficiency calculation:
+# node_eff_i = no_cores_used_in_node_i/node_i_chunk
+# total evaluation metric: node_eff_bottleneck * (1/(log(no_of_nodes) + 1))
+def eff_metric(total_ncpus, queue):
+    pass
+
+
+# Function that recommends both the CPU and queue specifications for the provided job script
+# Takes in an evaluation metric for how efficient resources at a queue are being utlized,
+# and the selects the queue based on that metric
+# If no metric specified, the default metric used will be the eff_metric function
+def recommend_all(metric=None):
+    pass
+
 
 def main():
-     # Check if intermediate log file exists
-    if (not os.path.exists(QUEUE_PROC_FILE)) or (len(sys.argv) >= 2 and sys.arv[1] == REFRESH_KEYWORD):
-        try:
-            QUEUE_PROC_FILE = subprocess.check_output([QUEUE_PROC_SCRIPT])
-        except FileNotFoundError:
-            print('Subroutine script ', QUEUE_PROC_SCRIPT, ' not found, terminating program')
-            sys.exit()
-
-    with open(QUEUE_PROC_FILE, 'r') as infile:
-        q_file = infile.read()
-        q_list = q_file.split('Queue:')
-
-        q_load = {}
-        for q in q_list:
-            q_lines = q.split('\n')
-        
-            # Remove disabled queues
-            if len(filter(lambda s: 'enabled = False' in s, q_lines)) > 0:
-                continue
-            
-            q_load[q_lines[0].strip()] = {}
-
-            # TODO: perform load evaluation
+    pass
 
 
 if __name__ == '__main__':
